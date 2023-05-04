ebbaa80312f491e627a300f739475b2eb47483d0
Lucky Marty <contact@luckymarty.fr>
2023-05-04 22:28:14 +0200

    🚀 LM01: Testing

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LoginController extends Controller
+class SignupController extends Controller
 {
     public function index()
     {
         return response()->json([
             'success' => true
         ]);
+    }
+
+    private function user(
+        string $firstname,
+        string $lastname,
+        string $email,
+        string $password
+    )
+    {
     }
 }
